c586dcc8650aed9b3c9cdd885dda5b9d5c21a2aa
amiraatalla <amiraatalla63@gmail.com>
2022-11-26 21:38:57 +0200

    javascript revision

---
+++ script.js
@@ -0,0 +1,72 @@
+// console.log("menna")
+// document.write(`Hello World`)
+// document.write(` hi menna`)
+// name = "romisa"
+// console.log('my name is ', name);
+// console.log(`my name is ${name}`);
+// alert("Hi in my website")
+
+
+// var let const
+const name = "amira"
+// name = "romissa"
+var nam = "hana"
+nam = "mena"
+
+console.log(name);
+console.log(nam);
+
+var age, email;
+var salary = 1000, job="teacher";
+console.log(job, salary);
+
+document.write('my job is ', job, ' my salary is ',salary)
+document.write(`   my job is ${job} and my salary is ${salary}`)
+
+
+
+job = 'engineer'
+job = "engineer"
+salary = 11
+console.log(salary);
+salary /= 3
+console.log(salary);
+salary = salary + 5
+console.log(salary);
+
+firstNAme = "menna "
+lastName = "Barakat"
+fullName = firstNAme + lastName
+console.log(fullName);
+
+console.log(5+6);
+
+console.log(typeof salary == 'string' );
+console.log(typeof salary =='number');
+
+
+
+age = 12
+// if(age == 13){
+//     alert(`my age is ${age}`)
+// }
+// else if(age > 15){
+//    alert(`you are longer`)
+// }
+// else if(age <13){
+//     alert(`you are smaller`)
+// }
+// else{
+//     alert(`hard luck`)
+// }
+
+switch(age){
+    case 12 : alert(`my age is ${age}`)
+    break
+    case 15 : alert(`you are longer`)
+    break
+    case 14:alert(`you are smaller`)
+    break
+    default:alert(`hard luck`)
+    break
+}
